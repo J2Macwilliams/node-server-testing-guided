dc7bf963937cb2e7dcfeae68bb60beaf3fdee8ec
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-19 10:56:01 -0800

    Follow along with the tests.

---
+++ api/server.spec.js
@@ -0,0 +1,52 @@
+const request = require('supertest');
+
+const server = require('./server');
+
+describe('server.js', function() {
+	describe('environment', function() {
+		it('should set environment to testing', () => {
+			expect(process.env.DB_ENV).toBe('testing');
+		});
+	});
+	describe('GET /', function() {
+		it('should return a 200 ok', function() {
+			//spin up the server
+			return request(server)
+				.get('/')
+				.then(res => {
+					expect(res.status).toBe(200);
+				});
+			//make GET request to /
+			//look at the http status code for the response
+		});
+		it('should return a JSON', function() {
+			return request(server)
+				.get('/')
+				.then(res => {
+					expect(res.type).toMatch(/json/i);
+				});
+		});
+		it("should return {api: 'up'}", function() {
+			return request(server)
+				.get('/')
+				.then(res => {
+					expect(res.body.api).toMatch('up');
+				});
+        });
+        it.skip('sample of setting header', function() {
+            return request(server)
+                .post('/login')
+                .send({username: 'me', password: 'pass'})
+				.then(res => {
+                    const token = res.body.token;
+                    return request(server)
+                    .get('/')
+                    .set("Authorization", token)
+				    .then(res => {
+expect(res.status)
+					expect(Array.isArray(res.body)).toBe(true);
+                });
+            });
+        });
+	});
+});
